9cdf5a04ad7c5f14b16f1a256fc0ae9be5c34155
Isha Jajoo <ishajajoo1992@gmail.com>
2023-06-16 15:33:50 -0700

    first commit

---
+++ Day_1_Band_Name_generator.py
@@ -0,0 +1,5 @@
+## Code for Band Name Generator
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grow up in?\n")
+name = input("What is the name of your first pet\n")
+print("Your band name is {} {}".format(city, name))
